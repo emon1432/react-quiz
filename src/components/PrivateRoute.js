d0ef765349b1d81feae9b6b53b958d2b0ca9ebbb
emon1432 <ovirajemon143@gmail.com>
2022-12-31 00:28:15 +0600

    Route Redirection complete

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function PrivateRoute({ element }) {
+  const { currentUser } = useAuth();
+
+  return currentUser ? element : <Navigate to="/login" />;
+}
